cfb066fd9ab9b873314a380cce722ead3d27ce14
Sa18-f <sisazid182713@gmail.com>
2024-09-10 23:44:54 -0600

    associative array added with problem1

--- AssociativeArray.php/problem1.php
+++ AssociativeArray.php/problem1.php
@@ -5,3 +5,13 @@ Create a PHP script which displays the capital and country name from the above a
 Sample Output :
 The capital of Netherlands is Amsterdam  
 The capital of Germany is Berlin  -->
+
+<?php 
+$ceu = array( "Italy"=>"Rome", "Luxembourg"=>"Luxembourg", "Belgium"=> "Brussels", "Denmark"=>"Copenhagen", "Finland"=>"Helsinki", "France" => "Paris", "Slovakia"=>"Bratislava", "Slovenia"=>"Ljubljana", "Germany" => "Berlin", "Greece" => "Athens", "Ireland"=>"Dublin", "Netherlands"=>"Amsterdam", "Portugal"=>"Lisbon", "Spain"=>"Madrid", "Sweden"=>"Stockholm", "United Kingdom"=>"London", "Cyprus"=>"Nicosia", "Lithuania"=>"Vilnius", "Czech Republic"=>"Prague", "Estonia"=>"Tallin", "Hungary"=>"Budapest", "Latvia"=>"Riga", "Malta"=>"Valetta", "Austria" => "Vienna", "Poland"=>"Warsaw") ;
+
+asort($ceu);
+foreach($ceu as $country => $capital){
+echo "The capital of $country is $capital" . "<br>";
+}
+
+?>
